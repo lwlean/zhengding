3433283f63e013921fb0223459a97eabb988cd53
lwlean <lean123321@163.com>
2020-06-02 17:29:59 +0800

    update upload file

--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,78 @@ var os = require('os');
 var router = express.Router();
 var Student = require('../model/Student');
 
+const UploadStu = async (oPath, nPath, fileName,res) => {
+    try {
+        var newFilePath = await RenameFile(oPath, nPath);
+        await ReadUploadFile(newFilePath, fileName, res);
+        await UploadSuccess(res);
+    } catch (e) {
+        e();
+        res.send({code: 500});
+    }
+}
+
+const RenameFile = (oldPath, newPath) => {
+    return new Promise((resolve, reject) => {
+        fs.rename(oldPath, newPath, (err) => {
+            if (err) {
+                console.log('rename error')
+                reject(() => console.log('rename file error'));
+            }
+            resolve(newPath);
+        });
+    });
+}
+
+const UploadSuccess = async (res) => {
+    console.log('upload success!!!');
+    res.send({code: 200});
+} 
+
+const ReadUploadFile = (newPath, fileName, res) => {
+    return new Promise((resolve, reject) => {
+        try{
+            fs.readFile(newPath, 'utf-8', (err, data) => {
+                if (err) {
+                    reject(() => console.log('read file error'));
+                }
+                for(let stu of data.split('\r\n')){
+                    if (stu.trim() === null || stu.trim() === ''){
+                        console.log('empty line >>>>>>>>>>>>');
+                        continue;
+                    }
+                    if(stu.indexOf(',') < 0) continue;
+                    // let tempStu = new Student();
+                    let name = stu.split(',')[0];
+                    let weight = Number.parseFloat(stu.split(',')[1]);
+                    let stu_class = fileName;
+                    
+                    Student.findOneAndRemove({name: name}).then(() => {
+                        console.log('remove successs');
+                        let tempStu = new Student();
+                        tempStu.name = name;
+                        tempStu.weight = weight;
+                        tempStu.class = stu_class;
+                        tempStu.save().then(() => {
+                            console.log('save success name is', name);
+                        }).catch(() => {
+                            reject(() => { console.log('save faild err'); });
+                            console.log('save faild err');
+                        });
+                    }).catch(() => { 
+                        reject(() => console.log('remove error >>>>'));
+                        console.log('rmove error >>>');
+                    });
+                }
+                resolve();
+            });
+        } catch (e) {
+            reject(() => {'read file error >>>'});
+        }
+        
+    })
+}
+
 router.post('/', function(req, res, next) {
     try{
         console.log('into file update....');
@@ -16,65 +88,12 @@ router.post('/', function(req, res, next) {
                 console.log('parse error');
                 res.send({code: 500});
                 return;
-            }else {
-                console.log('parse success');
             }
+            console.log('parse success');
             var filePath = files.file[0].path;
             var fileName = files.file[0].originalFilename;
             var savePath = './temp/' + fileName
-            new Promise((resolve, reject) => {
-                fs.rename(filePath, savePath, (err) => {
-                    if (err) {
-                        reject('renmae error')
-                        return;
-                    }
-                    resolve(savePath);
-                });
-            }).then((newPath) => {
-                fs.readFile(newPath, 'utf8', (err, data) => {
-                    if(err) {
-                        console.log(err);
-                        return;
-                    }
-                    // console.log(data);
-                    var stuArr = [];
-                    
-                    for(let stu of data.split('\r\n')){
-                        let student = {};
-                        if (stu.trim() === null || stu.trim() === ''){
-                            console.log('empty line >>>>>>>>>>>>');
-                            continue;
-                        }
-                        // console.log(stu);
-                        if(stu.indexOf(',') < 0) continue;
-                        var tempStu = new Student();
-                        tempStu.name = stu.split(',')[0];
-                        tempStu.weight = Number.parseFloat(stu.split(',')[1]);
-                        tempStu.class = fileName;
-                        // 先查询再插入
-                        const stuFind = queryStu(tempStu.name);
-                        console.log('fild stuFind', stuFind);
-                        
-                        // tempStu.save((err) => {
-                        //     if (err){
-                        //         console.log('save student err', err);
-                        //     } else {
-                        //         console.log('save student success:');
-                        //     }
-                        // });
-                    }
-                    new Promise((resolve, reject) => {
-                        resolve('read over!!!');
-                    });
-                });
-            }).then((msg) => {
-                console.log(msg);
-            }).catch((rst) => {
-                console.log(rst);
-                res.send({code: 500});
-            })
-            
-            
+            UploadStu(filePath, savePath, fileName, res);
         });
         
     }catch(e){
@@ -85,9 +104,3 @@ router.post('/', function(req, res, next) {
   });
 
   module.exports = router;
-
- async function queryStu(name){
-    var stu = await Student.findOne({ name: name});
-    console.log('find stuent:', stu);
-    return stu;
- } 
